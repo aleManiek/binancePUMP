6dafaa70a57727b119c734cb0155ac5077666d79
Maniek <mariusz.aleszko@gmail.com>
2021-02-24 21:59:22 +0100

    updated

--- buy.js
+++ buy.js
@@ -6,6 +6,7 @@ const url = process.env.URL;
 const apiKey = process.env.APIKEY;
 const secret = process.env.SECRET;
 const amount = process.env.AMOUNT;
+const estimatedProfit = process.env.ESTIMATED_PROFIT;
 
 const ticker = process.argv[2];
 const symbol = `${ticker}BTC`;
@@ -22,12 +23,15 @@ async function createMarketOrderAndPlaceTP() {
     });
     const data = await response.json();
     const { fills, executedQty } = data;
+    console.log(data);
     const price = parseFloat(fills[fills.length - 1].price);
-    console.log(`Kupiono ${executedQty} po cenie ${price.toLocaleString()}`);
+    console.log(`Kupiono ${executedQty} po cenie ${price}`);
 
     if (data.status === "FILLED") {
-      const newQs = `symbol=${symbol}&price=${price * 2}&quantity=${parseFloat(executedQty)}&timeInForce=GTC&side=SELL&type=LIMIT&timestamp=${Date.now()}`;
+      const newPrice = (price * estimatedProfit).toFixed(8);
+      const newQs = `symbol=${symbol}&price=${newPrice}&quantity=${parseFloat(executedQty)}&timeInForce=GTC&side=SELL&type=LIMIT&timestamp=${Date.now()}`;
       const signature = crypto.createHmac("sha256", secret).update(newQs).digest("hex");
+      console.log(`Próba wwystawienia zlecenia po cenie: ${price}`);
       const res = await fetch(`${url}?${newQs}&signature=${signature}`, {
         method: "post",
         headers: {
@@ -35,6 +39,7 @@ async function createMarketOrderAndPlaceTP() {
         },
       });
       const newData = await res.json();
+      console.log(newData);
       if (res.status === 200) console.log(`Wystawiono ${newData.origQty} po cenie ${newData.price}`);
     }
   } catch (err) {
